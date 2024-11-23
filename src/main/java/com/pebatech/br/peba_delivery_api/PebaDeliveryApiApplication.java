23054d5289bed1feeb014718e71c40c0d37ab728
Antonio Carlos S P <antoniocsprudencio@gmail.com>
2024-11-23 14:53:21 -0300

    primeiro commit - configuração inicial

---
+++ src/main/java/com/pebatech/br/peba_delivery_api/PebaDeliveryApiApplication.java
@@ -0,0 +1,13 @@
+package com.pebatech.br.peba_delivery_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PebaDeliveryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PebaDeliveryApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pebatech/br/peba_delivery_api/PebaDeliveryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pebatech.br.peba_delivery_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PebaDeliveryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
